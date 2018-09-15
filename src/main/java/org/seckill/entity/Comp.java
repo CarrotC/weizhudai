ddb5050db29e126e81b4d6b8fe835df00649c4df
iss2016302580196 <1305784537@qq.com>
2018-09-15 19:08:01 +0800

    添加comp表

---
+++ src/main/java/org/seckill/entity/Comp.java
@@ -0,0 +1,178 @@
+package org.seckill.entity;
+
+import java.util.Date;
+
+public class Comp {
+    private String compId;
+    private String compName;
+    private String legalRepresentative;
+    private String status;
+    private Double registeredCapital;
+    private Double paidInCapital;
+    private String compType;
+    private Integer peopleNo;
+    private String socialCreditCode;
+    private String businessRegistrationNumber;
+    private String organizationCode;
+    private String compNameEn;
+    private String industry;
+    private String bussinessScope;
+    private String operatingProcedure;
+    private String addr;
+    private String operatingPeriod;
+    private Date dateOfApproval;
+    private String registrationAuthority;
+
+
+    public String getCompId() {
+        return compId;
+    }
+
+    public void setCompId(String compId) {
+        this.compId = compId;
+    }
+
+    public String getCompName() {
+        return compName;
+    }
+
+    public void setCompName(String compName) {
+        this.compName = compName;
+    }
+
+    public String getLegalRepresentative() {
+        return legalRepresentative;
+    }
+
+    public void setLegalRepresentative(String legalRepresentative) {
+        this.legalRepresentative = legalRepresentative;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getRegisteredCapital() {
+        return registeredCapital;
+    }
+
+    public void setRegisteredCapital(Double registeredCapital) {
+        this.registeredCapital = registeredCapital;
+    }
+
+    public Double getPaidInCapital() {
+        return paidInCapital;
+    }
+
+    public void setPaidInCapital(Double paidInCapital) {
+        this.paidInCapital = paidInCapital;
+    }
+
+    public String getCompType() {
+        return compType;
+    }
+
+    public void setCompType(String compType) {
+        this.compType = compType;
+    }
+
+    public Integer getPeopleNo() {
+        return peopleNo;
+    }
+
+    public void setPeopleNo(Integer peopleNo) {
+        this.peopleNo = peopleNo;
+    }
+
+    public String getSocialCreditCode() {
+        return socialCreditCode;
+    }
+
+    public void setSocialCreditCode(String socialCreditCode) {
+        this.socialCreditCode = socialCreditCode;
+    }
+
+    public String getBusinessRegistrationNumber() {
+        return businessRegistrationNumber;
+    }
+
+    public void setBusinessRegistrationNumber(String businessRegistrationNumber) {
+        this.businessRegistrationNumber = businessRegistrationNumber;
+    }
+
+    public String getOrganizationCode() {
+        return organizationCode;
+    }
+
+    public void setOrganizationCode(String organizationCode) {
+        this.organizationCode = organizationCode;
+    }
+
+    public String getCompNameEn() {
+        return compNameEn;
+    }
+
+    public void setCompNameEn(String compNameEn) {
+        this.compNameEn = compNameEn;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getBussinessScope() {
+        return bussinessScope;
+    }
+
+    public void setBussinessScope(String bussinessScope) {
+        this.bussinessScope = bussinessScope;
+    }
+
+    public String getOperatingProcedure() {
+        return operatingProcedure;
+    }
+
+    public void setOperatingProcedure(String operatingProcedure) {
+        this.operatingProcedure = operatingProcedure;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getOperatingPeriod() {
+        return operatingPeriod;
+    }
+
+    public void setOperatingPeriod(String operatingPeriod) {
+        this.operatingPeriod = operatingPeriod;
+    }
+
+    public Date getDateOfApproval() {
+        return dateOfApproval;
+    }
+
+    public void setDateOfApproval(Date dateOfApproval) {
+        this.dateOfApproval = dateOfApproval;
+    }
+
+    public String getRegistrationAuthority() {
+        return registrationAuthority;
+    }
+
+    public void setRegistrationAuthority(String registrationAuthority) {
+        this.registrationAuthority = registrationAuthority;
+    }
+}
